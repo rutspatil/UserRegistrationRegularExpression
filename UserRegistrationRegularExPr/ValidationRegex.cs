195f5935a99ff219265a1c1e92e29099b1eec204
rutuja patil <rutujapatil991@gmail.com>
2022-03-18 15:09:01 +0530

    [RUTUJA]Adding Three Password

--- UserRegistrationRegularExPr/ValidationRegex.cs
+++ UserRegistrationRegularExPr/ValidationRegex.cs
@@ -44,7 +44,9 @@ namespace UserRegistrationRegularExPr
         public void Checkpassward(string Passward)
         {
             //string passwardpattern = "^[A-Za-z0-9]{8,20}$";
-            string passwardpattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}";
+            //string passwardpattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}";
+            //string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
